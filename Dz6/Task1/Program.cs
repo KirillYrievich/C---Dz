11db5b226af4fcb051fd89d0ebb05e5159172905
KirillYrievich <afanas.lg2@gmail.com>
2023-04-15 13:05:12 +0300

    Added new repository with homework 6

---
+++ Dz6/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Input any numbers separated by a space: ");
+string strNumbers = System.Console.ReadLine();
+string inputString = strNumbers.ToString();
+int [] ints = inputString.Split(' ').Select(Int32.Parse).ToArray();
+int count = 0;
+
+void Task1(int [] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {   
+        System.Console.Write($"{array[i]}; ");
+        if (array[i] > 0)
+        {
+            count ++;
+        }
+    }
+    System.Console.WriteLine($"-> {count}");
+    return;
+}
+
+Task1(ints);
