34adb38566b25aa79f22f87603202f7fd5a94539
KirillYrievich <afanas.lg2@gmail.com>
2023-05-02 21:21:24 +0300

    Added solution final test

--- Final test/Program.cs
+++ Final test/Program.cs
@@ -8,3 +8,43 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+string [] originalnArray = {"Hello", "World", "123456789", "^_^", "C#", };
+string [] resultArray = new string[CountElArray(originalnArray)];
+int count = 0;
+
+for (int i = 0; i < originalnArray.Length; i++)
+{
+    if (originalnArray[i].Length <= 3) 
+    {
+        resultArray[count] = originalnArray[i];
+        count ++;
+    }
+}
+
+
+PrintArray(resultArray);
+
+
+void PrintArray(string [] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length -1; i++)
+    {
+        System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+
+int CountElArray(string [] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+            if (array[i].Length <= 3) count ++;
+    }
+    return count;
+}
